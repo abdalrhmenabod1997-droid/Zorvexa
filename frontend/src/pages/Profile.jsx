079b2393ae24112217bde10e56947b42aae68b27
abdalrhmenabod1997-droid <abdalrhmen.abod1997@gmail.com>
2026-07-14 19:28:23 +0300

    Create Profile.jsx

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,15 @@
+export default function Profile() {
+  return (
+    <div className="container">
+      <h1>الملف الشخصي</h1>
+
+      <div className="card">
+        <h3>بيانات المستخدم</h3>
+
+        <p>الاسم: --</p>
+        <p>البريد الإلكتروني: --</p>
+        <p>عدد المقالات: 0</p>
+      </div>
+    </div>
+  );
+}
